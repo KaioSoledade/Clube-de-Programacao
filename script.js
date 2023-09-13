255fdfb22ef04d6166a5623c485a7dd7e808f142
Kaio Soledade <soledadekaio@gmail.com>
2023-09-12 23:38:23 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-
-
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector(".logo");
+    const imageButtons = document.querySelectorAll(".image-button");
+    let imagamIndex = 0;
+    
   
+
+
+
+    function selecaoimagembutao(imageURL) {
+      logo.style.backgroundImage = `url(${imageURL})`;
+  
+      imageButtons.forEach(button => {
+        const buttonImageURL = button.getAttribute("data-image");
+        if (buttonImageURL === imageURL) {
+          button.classList.add("active");
+        } else {
+          button.classList.remove("active");
+        }
+      });
+    }
+  
+
+
+
+    imageButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        const imageURL = this.getAttribute("data-image");
+     selecaoimagembutao(imageURL);
+        imagamIndex = Array.from(imageButtons).indexOf(this);
+      });
+    });
+
+
+    
+  
+    function autoChangeImage() {
+      imagamIndex = (imagamIndex + 1) % imageButtons.length;
+      const nextImageURL = imageButtons[imagamIndex].getAttribute("data-image");
+      selecaoimagembutao(nextImageURL);
+    }
+  
+    selecaoimagembutao("clube-banner.png");
+  
+    setInterval(autoChangeImage, 5000);
+  });
+  
+  
+
 
 var CardPlanet1 = document.getElementById('imagem1');
 var CardPlanet2 = document.getElementById('imagem2');
